7bb559c280e64add67388c95759d3d02c0fa5e10
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-02-22 14:10:52 +0530

    updated/added files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Pose-Estimation
 #### computer vision Pose estimation with mediapipe and opencv
+
+Pose Landmarker model
+![Pose Landmarker model](Pose-Estimation/pose-landmarker-model.png)
--- min.py
+++ min.py
@@ -9,7 +9,7 @@ mpDraw = mp.solutions.drawing_utils # creating object of drawing
 mpPose = mp.solutions.pose # creating object of pose
 pose = mpPose.Pose() # creating object of pose
 
-cap = cv2.VideoCapture("Pose-Estimation/videos/1.mp4")
+cap = cv2.VideoCapture("Pose-Estimation/videos/6.mp4")
 pTime = 0
 
 while True:
@@ -18,7 +18,12 @@ while True:
     results = pose.process(imgRGB) # processing the image
     #print(results.pose_landmarks) # printing the landmarks
     if results.pose_landmarks:
-        mpDraw.draw_landmarks(img, results.pose_landmarks) # drawing the landmarks
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS) # drawing the landmarks
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm) 
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
